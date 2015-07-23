e62cbc24715b080d911b48e234ceca625e0dfd71
Robin Appelman <icewind@owncloud.com>
2015-07-23 14:19:11 +0200

    add icon

--- appinfo/app.php
+++ appinfo/app.php
@@ -8,6 +8,6 @@ $c = \OC::$server;
 		'order' => 22,
 		'name' => 'Log reader',
 		'href' => \OC::$server->getURLGenerator()->linkToRoute('logreader.page.index'),
-		'icon' => \OC::$server->getURLGenerator()->imagePath('audios', 'app.svg'),//TODO
+		'icon' => \OC::$server->getURLGenerator()->imagePath('logreader', 'app.svg'),//TODO
 	];
 });
