1db605edaf14097e107a234bce9fd1311d8e2751
Robin Appelman <icewind@owncloud.com>
2015-08-25 23:21:38 +0200

    hide nav entry when not admin

--- appinfo/app.php
+++ appinfo/app.php
@@ -2,12 +2,15 @@
 
 $c = \OC::$server;
 
-\OC::$server->getNavigationManager()->add(function () {
-	return [
-		'id' => 'logreader',
-		'order' => 22,
-		'name' => 'Log reader',
-		'href' => \OC::$server->getURLGenerator()->linkToRoute('logreader.page.index'),
-		'icon' => \OC::$server->getURLGenerator()->imagePath('logreader', 'app.svg'),//TODO
-	];
-});
+$user = \OC::$server->getUserSession()->getUser();
+if (\OC::$server->getGroupManager()->isAdmin($user->getUID())) {
+	\OC::$server->getNavigationManager()->add(function () {
+		return [
+			'id' => 'logreader',
+			'order' => 22,
+			'name' => 'Log reader',
+			'href' => \OC::$server->getURLGenerator()->linkToRoute('logreader.page.index'),
+			'icon' => \OC::$server->getURLGenerator()->imagePath('logreader', 'app.svg'),//TODO
+		];
+	});
+}
