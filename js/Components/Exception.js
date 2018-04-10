3a2b191082c05a5fe091ef890c1a594029770c1c
Robin Appelman <robin@icewind.nl>
2018-04-10 11:45:51 +0200

    show previous exceptions
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- js/Components/Exception.js
+++ js/Components/Exception.js
@@ -15,29 +15,39 @@ export class Exception extends Component {
 	};
 
 	render () {
-		var traceElements;
-		if (this.state.expanded) {
-			traceElements = this.props.Trace.map((trace, i) => {
-				return (
-					<TraceLine key={i} {...trace}/>
-				);
-			});
-			traceElements = (
-				<ol className={style.trace} start="0">
-					{traceElements}
-				</ol>
-			)
-		} else {
-			traceElements = [];
-		}
+		const expanded = this.state.expanded || this.props.expanded;
 		return (
 			<span
-				className={style.exceptionRow + ' icon-caret-dark'}
+				className={[style.exceptionRow, (this.props.isPrevious ? style.previous : 'icon-caret-dark')].join(' ')}
 				onClick={this.clickHandler}>
-				<span className={style.exception}>{this.props.Exception}</span>:&nbsp;
-				<span className={style.messsage}>{this.props.Message}</span>
-				{traceElements}
+				<span className={style.exception}>
+					{this.props.isPrevious ? t('logreader', 'Cased by ') : ''}
+					{this.props.Exception}
+				</span>:&nbsp;
+				<span className={style.message}>{this.props.Message}</span>
+				<StackTrace trace={this.props.Trace}
+							expanded={expanded}/>
+				{expanded && this.props.Previous ? [
+					<Exception expanded={true} {...this.props.Previous}
+							   isPrevious={true}/>
+				] : []}
 			</span>
 		);
+	}
+}
+
+function StackTrace ({trace, expanded}) {
+	if (expanded) {
+		return (
+			<ol className={style.trace} start="0">
+				{trace.map((trace, i) => {
+					return (
+						<TraceLine key={i} {...trace}/>
+					);
+				})}
+			</ol>
+		)
+	} else {
+		return [];
 	}
 }
