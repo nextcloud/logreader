5be1df62588f548c470ca6f763cfbdbaf101e59a
Robin Appelman <robin@icewind.nl>
2020-05-04 16:43:33 +0200

    remove navigation fallback for ancient nc versions

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,16 +1,1 @@
 <?php
-
-$c = \OC::$server;
-
-$user = \OC::$server->getUserSession()->getUser();
-if ($user and \OC::$server->getGroupManager()->isAdmin($user->getUID()) and !interface_exists('\OCP\Settings\ISettings')) {
-	\OC::$server->getNavigationManager()->add(function () {
-		return [
-			'id' => 'logreader',
-			'order' => 22,
-			'name' => 'Log reader',
-			'href' => \OC::$server->getURLGenerator()->linkToRoute('logreader.page.index'),
-			'icon' => \OC::$server->getURLGenerator()->imagePath('logreader', 'app.svg'),//TODO
-		];
-	});
-}
