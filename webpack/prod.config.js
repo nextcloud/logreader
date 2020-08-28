efcb55ed4e62717017213aaa963df71221ffb823
Robin Appelman <robin@icewind.nl>
2020-08-28 16:31:12 +0200

    bumb dependencies

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -53,7 +53,7 @@ module.exports = {
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin("[name].css"),
+		new MiniCssExtractPlugin({filename: "[name].css"}),
 		new webpack.DefinePlugin({
 			__CLIENT__: true,
 			__SERVER__: false,
