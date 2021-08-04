d9798a314de48e2b2a46c2fce8c0f245e14f7c3e
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-08-04 20:15:13 +0200

    l10n: Change dot to ellipsis
    Signed-off-by: Valdnet <47037905+Valdnet@users.noreply.github.com>

--- js/Components/LogUploader.js
+++ js/Components/LogUploader.js
@@ -5,7 +5,7 @@ import style from './LogUploader.css';
 
 export class LogUploader extends Component {
 	state = {
-		message: t('logreader', 'Load log file...')
+		message: t('logreader', 'Load log file …')
 	};
 
 	isLog (content) {
