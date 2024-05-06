ffaacb0078aa584ea892942564e874ef10e9f354
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-06 16:48:54 +0200

    chore: Unified SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- tests/Unit/Service/SettingsServiceTest.php
+++ tests/Unit/Service/SettingsServiceTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 /**
- * SPDX-FileCopyrightText: 2023 Ferdinand Thiessen <opensource@fthiessen.de>
+ * SPDX-FileCopyrightText: 2023 Nextcloud Gmbh and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
