0696aa48b0020f15b14884764f0284d0e56c8196
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-06 16:58:05 +0200

    chore: Update SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,9 +1,9 @@
 <?php
 /**
+ * SPDX-FileCopyrightText: 2016-2024 Nextcloud GmbH and Nextcloud contributors
  * SPDX-FileCopyrightText: 2015 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-
 namespace OCA\LogReader\Controller;
 
 use OCA\LogReader\Service\SettingsService;
