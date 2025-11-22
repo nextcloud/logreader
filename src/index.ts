0bf831d93286f67dbe52a3bc9ae6f387e450cfad
Maksim Sukharev <antreesy.web@gmail.com>
2025-11-22 14:40:42 +0100

    chore: update tsconfig and mount file to Vue 3
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -3,32 +3,12 @@
 * SPDX-License-Identifier: AGPL-3.0-or-later
 */
 
-import { translate, translatePlural } from '@nextcloud/l10n'
-import {
-	type Pinia,
-
-	createPinia,
-	PiniaVuePlugin,
-} from 'pinia'
-import Vue from 'vue'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import App from './App.vue'
-
-Vue.use(PiniaVuePlugin)
-Vue.mixin({ methods: { t: translate, n: translatePlural } })
-Vue.prototype.t = translate
-Vue.prototype.n = translatePlural
 
 const pinia = createPinia()
 
-// ComponentOptions is declared in types/options.d.ts
-declare module 'vue/types/options.d.ts' {
-	interface ComponentOptions<V extends Vue> {
-		pinia?: Pinia
-	}
-}
-
-const mainApp = new Vue({
-	el: '#logreader-root',
-	render: (h) => h(App),
-	pinia,
-})
+createApp(App)
+	.use(pinia)
+	.mount('#app-content')
