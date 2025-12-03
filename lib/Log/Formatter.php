282a78d899b06ad15d8f575bab4e2aef900f02a7
Ivan Sein <ivan@nextcloud.com>
2025-12-03 17:58:04 +0100

    fix: remove unnecessary whitespaces in stack trace
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- lib/Log/Formatter.php
+++ lib/Log/Formatter.php
@@ -51,7 +51,7 @@ class Formatter {
 
 		$argumentsString = implode(', ', $arguments);
 		$argumentWhiteSpace = str_repeat(' ', $largestIndexWidth + 2);
-		if ($argumentsString && strlen($argumentsString) < $argumentWidth) {
+		if ($argumentsString === '' || strlen($argumentsString) < $argumentWidth) {
 			return $whiteSpace . $index . '. ' . $this->getFileAndLine($trace, $argumentWidth) . "\n" .
 				$argumentWhiteSpace . $method . '(' .
 				$argumentsString . ')';
