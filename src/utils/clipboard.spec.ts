1981def6d00172ee0dfc1e61d7f13010d42727c5
Maksim Sukharev <antreesy.web@gmail.com>
2026-07-11 08:44:02 +0200

    fix: adjust test behaviour
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/utils/clipboard.spec.ts
+++ src/utils/clipboard.spec.ts
@@ -7,7 +7,7 @@ import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { copyToCipboard } from './clipboard'
 
 describe('utils:clipboard', () => {
-	let originalWindowPrompt: window.prompt
+	const originalWindowPrompt = window.prompt
 
 	beforeEach(() => {
 		window.prompt = vi.fn(() => '')
