e737571a87563eb1fccd2f4f25b61c5237f54ebe
Côme Chilliet <come.chilliet@nextcloud.com>
2026-09-23 12:01:03 +0200

    fix: Remove legacy method calls from tests/bootstrap.php
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,19 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 /**
- * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: CC0-1.0
+ * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-if (!defined('PHPUNIT_RUN')) {
-	define('PHPUNIT_RUN', 1);
-}
+use OCP\App\IAppManager;
+use OCP\Server;
+
+define('PHPUNIT_RUN', 1);
 
 require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 
-\OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
-\OC::$composerAutoloader->addPsr4('Tests\\', OC::$SERVERROOT . '/tests/', true);
-
-OC_App::loadApp('logreader');
-
-OC_Hook::clear();
+Server::get(IAppManager::class)->loadApp('logreader');
